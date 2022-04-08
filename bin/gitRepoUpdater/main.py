d2e0e6d9cd6494bd66a28d2942a898e894fa3b3b
leon3131-ux <knud.del@hotmail.com>
2022-04-08 16:05:29 +0200

    refactoring

--- bin/gitRepoUpdater/main.py
+++ bin/gitRepoUpdater/main.py
@@ -1,4 +1,5 @@
-import logging
+import argparse
+import sys
 from collections import namedtuple
 from datetime import datetime
 
@@ -8,26 +9,56 @@ CommandLineArguments = namedtuple("CommandLineArguments", "repo_file_path base_d
 
 
 def init_logger(verbose: bool):
-    log_formatter = logging.Formatter("%(asctime)s - %(levelname): %(message)s")
+    log_formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
     root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
 
-    file_handler = logging.FileHandler(datetime.today().strftime('%Y_%m_%d_%H:%M:%S') + ".log")
+    file_handler = logging.FileHandler(datetime.today().strftime('%Y_%m_%d_%H-%M-%S') + ".log")
     file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(logging.INFO)
     root_logger.addHandler(file_handler)
 
     if verbose:
-        console_handler = logging.StreamHandler()
+        console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(log_formatter)
-        root_logger.addHandler(file_handler)
+        console_handler.setLevel(logging.INFO)
+        root_logger.addHandler(console_handler)
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(prog='gitRepoUpdater',
+                                     description='automatically clones or updates git repositories')
+    parser.add_argument('--repos',
+                        help='Absolute or relative path to the txt file containing the repo list',
+                        type=str,
+                        required=True)
+    parser.add_argument('--base',
+                        help='Absolute or relative path to the base directory containing all the repos',
+                        type=str,
+                        required=True)
+    parser.add_argument('--v',
+                        help='Verbose, if set all the logfile output will also be present in console',
+                        action='store_true',
+                        required=False)
+    return parser
 
 
 def main():
-    command_line_arguments = get_valid_input_arguments()
-    repo_file_path = command_line_arguments.repo_file_path
-    base_dir = command_line_arguments.base_dir
-    verbose = command_line_arguments.verbose
+    arg_parser = init_argparse()
+    args = arg_parser.parse_args()
+    repo_file_path = args.repos
+    base_dir = args.base
+    verbose = args.v
 
     init_logger(verbose)
+
+    if not repo_file_is_valid(repo_file_path):
+        logger.error("repo file: {0} is invalid".format(repo_file_path))
+        sys.exit()
+
+    if not os.path.isdir(base_dir):
+        os.mkdir(base_dir)
+
     run(repo_file_path, base_dir)
 
 
