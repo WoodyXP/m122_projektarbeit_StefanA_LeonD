9ec86dfbdcf624cc6ff9d0ab360a773f56ddb2a8
WoodyXP <55553527+WoodyXP@users.noreply.github.com>
2022-04-08 11:22:16 +0200

    Validation for the targetdict

--- bin/gitRepoUpdaterFunctions.py
+++ bin/gitRepoUpdaterFunctions.py
@@ -2,6 +2,7 @@ import shutil
 import validators
 import git
 import os
+import re
 
 
 def dir_is_repo(path):
@@ -33,12 +34,24 @@ def clone_repo(path, remote_url):
         return
 
 
+def remove_line_ending(value):
+    return value.replace('\n', '')
+
 def repo_file_is_valid(path):
     with open(path) as inputfile:
         giturl_list = [line.split(None, 1)[0] for line in inputfile]
+        targetdict_list = [line.split(None, 1)[1] for remove_line_ending(line) in inputfile]
+        targetdict_rl_list = []
+        for targetdicts in targetdict_list:
+            targetdict_rl_list.append(remove_line_ending(targetdicts))
         for giturl in giturl_list:
             validated_entry = validators.url(giturl)
             if validated_entry != True:
                print("This URL: " + giturl + " is invalid")
                ## TODO: Logging
+        for targetdicts in targetdict_rl_list:
+            if re.match("^[A-Za-z0-9_-]*$", targetdicts):
+                print("Targetdict name: " + targetdicts + " is valid")
+            else:
+                print("Targetdict name: " + targetdicts + " is invalid")
     inputfile.close()
