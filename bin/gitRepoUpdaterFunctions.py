9d9150002ef4bcafa3c56316acfc20ded989c87b
WoodyXP <55553527+WoodyXP@users.noreply.github.com>
2022-04-30 15:25:43 +0200

    Fixed file validation func

--- bin/gitRepoUpdaterFunctions.py
+++ bin/gitRepoUpdaterFunctions.py
@@ -37,21 +37,12 @@ def clone_repo(path, remote_url):
 def remove_line_ending(value):
     return value.replace('\n', '')
 
+
 def repo_file_is_valid(path):
-    with open(path) as inputfile:
-        giturl_list = [line.split(None, 1)[0] for line in inputfile]
-        targetdict_list = [line.split(None, 1)[1] for remove_line_ending(line) in inputfile]
-        targetdict_rl_list = []
-        for targetdicts in targetdict_list:
-            targetdict_rl_list.append(remove_line_ending(targetdicts))
-        for giturl in giturl_list:
-            validated_entry = validators.url(giturl)
-            if validated_entry != True:
-               print("This URL: " + giturl + " is invalid")
-               ## TODO: Logging
-        for targetdicts in targetdict_rl_list:
-            if re.match("^[A-Za-z0-9_-]*$", targetdicts):
-                print("Targetdict name: " + targetdicts + " is valid")
-            else:
-                print("Targetdict name: " + targetdicts + " is invalid")
-    inputfile.close()
+    with open(path) as f:
+        for line in f:
+            if not validators.url(line.split()[0]):
+                return False
+            if not re.match("^[A-Za-z0-9_-]*$", line.split()[1]):
+                return False
+    f.close()
